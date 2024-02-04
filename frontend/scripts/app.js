1c9680f762d351ccd7a60040fa026fdb3ee22b97
Курушин Федор Иванович <kurushin.fedor@wb.ru>
2024-02-04 18:47:37 +0300

    button works fine

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -1,4 +1,93 @@
 const record = document.querySelector(".record");
-const stop = document.querySelector(".stop");
+const stopButton = document.querySelector(".stop");
 const soundClips = document.querySelector(".sound-clips");
 
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    console.log("getUserMedia supported.");
+    // Navigator Интерфейc представляет состояние и особенности пользовательского агента. 
+    // Он позволяет скриптам запрашивать их и самостоятельно регистрироваться для выполнения 
+    // некоторых действий.
+    navigator.mediaDevices
+        .getUserMedia(
+            // if only audio needed
+            {
+                audio: true,
+            },
+        )
+    
+        // success callback
+        // Callback - это функция, которая передается в качестве аргумента в другую 
+        // функцию и вызывается после выполнения определенных действий. В данном случае, 
+        // `then()` и `catch()` являются методами объекта `Promise`, которые принимают функции
+        //  обратного вызова в качестве аргументов.
+        // pass стрелочную функцию 
+        // https://developer.mozilla.org/ru/docs/Web/API/MediaDevices/getUserMedia
+        .then((stream) => {
+            const mediaRecorder = new MediaRecorder(stream);
+            
+            visualize(stream);
+            
+            record.onclick = () => {
+                mediaRecorder.start();
+                console.log(mediaRecorder.state);
+                console.log("recorder started");
+                record.style.background = "red";
+                record.style.color = "black";
+            };
+            
+            let chunks = [];
+
+            mediaRecorder.ondataavailable = (e) => {
+                chunks.push(e.data);
+            };
+
+            stopButton.onclick = () => {
+                mediaRecorder.stop();
+                console.log(mediaRecorder.state);
+                console.log("recorder stopped");
+                record.style.background = "";
+                record.style.color = "";
+              };
+            
+            mediaRecorder.onstop = (e) => {
+            console.log("recorder stopped");
+            
+            const clipName = prompt("Enter a name for your sound clip");
+            
+            const clipContainer = document.createElement("article");
+            const clipLabel = document.createElement("p");
+            const audio = document.createElement("audio");
+            const deleteButton = document.createElement("button");
+            
+            clipContainer.classList.add("clip");
+            audio.setAttribute("controls", "");
+            deleteButton.innerHTML = "Delete";
+            clipLabel.innerHTML = clipName;
+            
+            clipContainer.appendChild(audio);
+            clipContainer.appendChild(clipLabel);
+            clipContainer.appendChild(deleteButton);
+            soundClips.appendChild(clipContainer);
+            
+            const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
+            chunks = [];
+            const audioURL = window.URL.createObjectURL(blob);
+            audio.src = audioURL;
+            
+            deleteButton.onclick = (e) => {
+                let evtTgt = e.target;
+                evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
+            };
+            };
+            
+
+        })
+
+        // error callback
+        .catch((err)  => {
+            console.error(`The following getUserMedia error occurred: ${err}`);
+        });
+} else {
+    console.log("getUserMEdia not supported on your browser!");
+}
+  
